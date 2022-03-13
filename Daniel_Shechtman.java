c1c64bf96deec816620f9c6df9597f0b597ecb27
dsisback <dsisback01@gmail.com>
2022-03-13 02:46:09 +0200

    first commit ex3

---
+++ Daniel_Shechtman.java
@@ -0,0 +1,9 @@
+public class Daniel_Shechtman
+{
+    public static void main(String[] args) {
+        for(int i = 0 ;i<5;i++){
+            System.out.println("daniel update number: "+(i+1));
+        }
+
+    }
+}
